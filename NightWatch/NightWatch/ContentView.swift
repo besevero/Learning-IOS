2fc79d78acb54255af6711ac20944dbe4bbce797
besevero <besevero@gmail.com>
2022-04-01 19:59:07 -0300

    List Creation

--- NightWatch/NightWatch/ContentView.swift
+++ NightWatch/NightWatch/ContentView.swift
@@ -7,12 +7,59 @@
 
 import SwiftUI
 
+let nightlyTasks = [
+    "-Check all windows",
+    "-Check all doors",
+    "-Check the mail box",
+    "-Inspect secutiry cameras",
+    "-Clear ice from sidewalks",
+    "-Document \"strange and unusual\" occurrences"
+]
+let weeklyTasks = [
+    "-Check inside all vacant rooms",
+    "-Walk the perimeter of property"
+]
+let monthlyTasks = [
+    "-Test security alarm",
+    "-Test motion Detectors",
+    "-Test smoke alarms"
+]
 struct ContentView: View {
     var body: some View {
-        VStack{
-            Text("Nightly Tasks")
-            Text("Weekly Tasks")
-            Text("Monthly Tasks")
+        
+        VStack {
+            List{
+                Section(header: HStack{
+                    Image(systemName: "moon.stars")
+                    Text("Nightly Tasks")
+                }
+                .font(.title3)){
+                    ForEach(nightlyTasks, id: \.self, content: {
+                        taskName in
+                        Text(taskName)
+                    })
+                }
+                Section(header: HStack{
+                    Image(systemName: "sunset")
+                    Text("Weekly Tasks")
+                }
+                .font(.title3)){
+                    ForEach(weeklyTasks, id: \.self, content: {
+                        taskName in
+                        Text(taskName)
+                    })
+                }
+                Section(header: HStack{
+                    Image(systemName: "calendar")
+                    Text("Monthly Tasks")
+                }
+                .font(.title3)){
+                    ForEach(monthlyTasks, id: \.self, content: {
+                        taskName in
+                        Text(taskName)
+                    })
+                }
+            }
         }
     }
 }
